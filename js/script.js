063e1d5b04e414e5c789ea5d5855616e4a9fc1ac
rashmikaz <phrashmika@gmail.com>
2024-05-08 12:08:52 +0300

    done few changes

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
-const sidebarToggle = document.querySelector("#sidebar-toggle");
-sidebarToggle.addEventListener("click",function(){
-    document.querySelector("#sidebar").classList.toggle("collapsed");
+// $("#login").css({display:'block'});
+// $("#dashboard-section").css({display: 'none'});
+// $("#customer-section").css({display: 'none'});
+// $("#item-section").css({display: 'none'});
+// $("#place-order-section").css({display: 'none'});
+//
+// $("#btn-signIn").on('click',() =>{
+//     $("#login").css({display:'none'});
+//     $("#dashboard-section").css({display: 'block'});
+// });
+
+$(".btn-dashboard").on('click',() =>{
+    $("#dashboard-section").css({display: 'block'});
 });
 
-document.querySelector(".theme-toggle").addEventListener("click",() => {
-    toggleLocalStorage();
-    toggleRootClass();
+$(".btn-customer").on('click',() =>{
+    $("#dashboard-section").css({display: 'none'});
+    $("#customer-section").css({display: 'block'});
 });
 
-function toggleRootClass(){
-    const current = document.documentElement.getAttribute('data-bs-theme');
-    const inverted = current == 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-bs-theme',inverted);
-}
+$(".btn-item").on('click',() =>{
+    $("#dashboard-section").css({display: 'none'});
+    $("#item-section").css({display: 'block'});
+});
 
-function toggleLocalStorage(){
-    if(isLight()){
-        localStorage.removeItem("light");
-    }else{
-        localStorage.setItem("light","set");
-    }
-}
+$(".place-order-section").on('click',() =>{
+    $("#dashboard-section").css({display: 'none'});
+    $("#place-order-section").css({display: 'block'});
+});
 
-function isLight(){
-    return localStorage.getItem("light");
-}
-
-if(isLight()){
-    toggleRootClass();
-}
+$(".btn-signOut").on('click',() =>{
+    $("#dashboard-section").css({display: 'none'});
+    $("#login").css({display: 'block'});
+});
