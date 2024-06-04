d32073785ac102fb83cde969c57380a59c22387b
rashmikaz <phrashmika@gmail.com>
2024-06-05 03:26:58 +0530

    few changes

--- js/pos.js
+++ js/pos.js
@@ -1,6 +1,4 @@
-
-
-$('#login-section').css({display:'block'});
+$('#login-sec').css({display:'block'});
 $('#home-sec').css({display:'none'});
 $('#customer-sec').css({display:'none'});
 $('#store-sec').css({display:'none'});
@@ -8,26 +6,33 @@ $('#order-sec').css({display:'none'});
 
 /*home-sec*/
 $('#nav-home').on('click',()=>{
-    $('#login-section').css({display:'none'});
+    $('#login-sec').css({display:'none'});
+    $('#store-sec').css({display:'none'});
+    $('#customer-sec').css({display:'none'});
+    $('#order-sec').css({display:'none'});
+    $('#home-sec').css({display:'block'});
+})
+$('#btnSign').on('click',()=>{
+    $('#login-sec').css({display:'none'});
     $('#store-sec').css({display:'none'});
     $('#customer-sec').css({display:'none'});
     $('#order-sec').css({display:'none'});
     $('#home-sec').css({display:'block'});
 })
 
-/*login-sec*/
+/*logoutBtn-login-sec*/
 $('#nav-login').on('click',()=>{
     $('#home-sec').css({display:'none'});
     $('#store-sec').css({display:'none'});
     $('#customer-sec').css({display:'none'});
     $('#order-sec').css({display:'none'});
-    $('#login-section').css({display:'block'});
+    $('#login-sec').css({display:'block'});
 })
 
 /*customer-sec*/
 $('#nav-customer').on('click',()=>{
     $('#home-sec').css({display:'none'});
-    $('#login-section').css({display:'none'});
+    $('#login-sec').css({display:'none'});
     $('#store-sec').css({display:'none'});
     $('#order-sec').css({display:'none'});
     $('#customer-sec').css({display:'block'});
@@ -35,20 +40,49 @@ $('#nav-customer').on('click',()=>{
 /*store-sec*/
 $('#nav-store').on('click',()=>{
     $('#home-sec').css({display:'none'});
-    $('#login-section').css({display:'none'});
+    $('#login-sec').css({display:'none'});
     $('#order-sec').css({display:'none'});
     $('#customer-sec').css({display:'none'});
     $('#store-sec').css({display:'block'});
 })
+/*order-sec*/
 $('#nav-order').on('click',()=>{
     $('#home-sec').css({display:'none'});
-    $('#login-section').css({display:'none'});
+    $('#login-sec').css({display:'none'});
+    $('#customer-sec').css({display:'none'});
+    $('#store-sec').css({display:'none'});
+    $('#order-sec').css({display:'block'});
+})
+
+/*--home section-side btn*/
+
+/*btn- customer*/
+$('#nav-btn-customer').on('click',()=>{
+    $('#home-sec').css({display:'none'});
+    $('#login-sec').css({display:'none'});
+    $('#store-sec').css({display:'none'});
+    $('#order-sec').css({display:'none'});
+    $('#customer-sec').css({display:'block'});
+})
+/*btn-store*/
+$('#nav-btn-store').on('click',()=>{
+    $('#home-sec').css({display:'none'});
+    $('#login-sec').css({display:'none'});
+    $('#order-sec').css({display:'none'});
+    $('#customer-sec').css({display:'none'});
+    $('#store-sec').css({display:'block'});
+})
+/*btn-orders*/
+$('#nav-btn-order').on('click',()=>{
+    $('#home-sec').css({display:'none'});
+    $('#login-sec').css({display:'none'});
     $('#customer-sec').css({display:'none'});
     $('#store-sec').css({display:'none'});
     $('#order-sec').css({display:'block'});
 })
 
 
+/*------------------------------------------*/
 var headerNav=document.getElementById('nav-element');
 headerNav.style.display='none';
 
